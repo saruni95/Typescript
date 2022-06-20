9b095963568c15917990b94c185368aca2be9c80
Sara Miragaya Rico <saramiragaya@gmail.com>
2022-06-20 15:48:14 +0200

    Examples 1 and 2

--- Proyecto Typescript/app.ts
+++ Proyecto Typescript/app.ts
@@ -1,1 +1,43 @@
-console.log("Tu codigo va aquí...");
+var exports = {};
+
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  /* if (typeof n1 !== "number" || typeof n2 !== "number") {
+    throw new Error("Incorrect input!");
+  } */
+
+  //EXAMPLE 1
+  const result = n1 + n2; //To make the result only a number
+  if (showResult) {
+    console.log(phrase + result); //If we use phrase + n1 + n2 the result is a String
+  } else {
+    //If showResult = false -> return n1 + n2
+    return n1 + n2;
+  }
+}
+
+let number1: number; // 5.0
+number1 = 5;
+
+const number2 = 2.8;
+const printResult = true;
+let resultPhrase = "Result is: ";
+
+//console.log(typeof number1);
+
+//const result = add(number1, number2, printResult);
+//console.log(result);
+
+add(number1, number2, printResult, resultPhrase);
+
+//EXAMPLE 2
+/* const person: {
+  // {} the empty curly braces mean the same as an object, we have to fill them
+  name: string;
+  age: number;
+} = { */
+const person = {
+  name: "Sara",
+  age: 26,
+};
+
+console.log(person.name);
