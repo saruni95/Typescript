a6276ccc0bd8ee94483664a17a5866239aa76e43
saruni95 <s.miragaya@inovalabs.es>
2022-06-27 08:48:36 +0200

    Trying

--- Proyecto Typescript/app.js
+++ Proyecto Typescript/app.js
@@ -1,5 +1,5 @@
 "use strict";
-let age;
-age = 30;
-const userName1 = "Sara";
-console.log(userName1);
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+    console.log("Clicked!");
+});
--- Proyecto Typescript/app.ts
+++ Proyecto Typescript/app.ts
@@ -1,9 +1,3 @@
-let age: number;
-age = 30;
-const userName1 = "Sara";
-
-console.log(userName1);
-
 const button = document.querySelector("button")!;
 
 button.addEventListener("click", () => {
